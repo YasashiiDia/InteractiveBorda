6a3a2e41327fdf4324ba8b1ae366f9dacbe96097
YasashiiDia <75679547+YasashiiDia@users.noreply.github.com>
2022-05-18 16:49:14 +0200

    some styling

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import streamlit as st
 
 import utility as ut
+import style as sty
 from options import options_dict_all
 
 TOP_WEIGHT_DISTRIBUTION = list(np.linspace(0.1, 0.9, 10)) + list(np.linspace(1, 5, 11))
@@ -29,6 +30,8 @@ def get_results_df(vote_matrix, top_weight, pop_weight, _pop_multiplier, size_de
     results["Score"] += 0.00001 * results["Votes"]  # hacky way of breaking ties by number of votes AND use method="min" for tied votes
     results["Rank"] = results["Score"].rank(ascending=False, method='min').astype(int)
     results["Score"] = results["Score"].round(1)
+    results.index.set_names(['ID', 'Title'], inplace=True)
+    results.sort_values(by="Rank", inplace=True)
     return results, ranked_vote_matrix
 
 
@@ -41,7 +44,7 @@ def load_data(**options):
 
 def main(**options):
 
-    ## Weights
+    # Weights
     top_weight = st.sidebar.slider('Top Weight', -10, 10, 0)
     top_weight = TOP_WEIGHT_DISTRIBUTION[top_weight + 10]
     pop_weight = 10*st.sidebar.slider('Pop Weight', -20, 20, 0)
@@ -52,21 +55,56 @@ def main(**options):
         pop_weight -= np.sign(pop_weight) * 100
         pop_multiplier = ut.elliptical_pop_multiplier
 
+    # Other widgets
     size_dependent_borda = st.sidebar.checkbox('Size-dependent Borda')
 
+    # Calculate results
     vote_matrix, meta_df = load_data(**options)
     results, rvm = get_results_df(vote_matrix, top_weight, pop_weight, pop_multiplier, size_dependent_borda)
 
-    return results
+    # Add metadata
+    metacols = options["metacols"] + ["IMGID"]
+    meta_df_display = meta_df[metacols]
+    results = pd.concat([results, meta_df_display], axis=1)
+
+    # Style results
+    cols = results.columns.to_list()
+    cols.remove("IMGID")
+    results["ID"] = results.index.get_level_values(level=0)
+    results["Title"] = results.index.get_level_values(level=1)
+    results.index = results["IMGID"].apply(sty.path_to_tmdb_image_html)
+    results.index.name = None
+    results = results[["Title"]+cols+["ID"]]
+
+    results = results[:20]
+    results_styled = results
+
+    # show_id = st.sidebar.checkbox('Show title ID')
+    # if not show_id:
+    #     results_styled = results_styled.hide(axis="index")  # doesn't work with st.dataframe()
+
+    with open("style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+    # https://stackoverflow.com/questions/50807744/apply-css-class-to-pandas-dataframe-using-to-html
+    # https://towardsdatascience.com/pagination-in-streamlit-82b62de9f62b
+    # https://docs.streamlit.io/library/components/components-api
+    # https://pagination.js.org/
+    # https://www.jqueryscript.net/blog/best-table-pagination.html
+
+    table = results_styled.to_html(classes="styled-table", escape=False)
+    st.write(table, unsafe_allow_html=True)
+
+    #results_styled = sty.style_df(results, metacols)
+    #st.dataframe(results_styled)
 
 
 if __name__ == '__main__':
 
-    st.title('RYM Interactive Poll Results')
+    st.title("RYM Interactive Poll Results")
+    st.write(" More features: [Google Colab](https://colab.research.google.com/drive/1hOq6fSF2a7t00FXl-KBUVlYifpz9ZkHp)")
 
     dataset = st.sidebar.selectbox('Select Dataset', options_dict_all.keys())
     options_dict = options_dict_all[dataset]
 
-    results = main(**options_dict)
-
-    st.dataframe(results)
+    main(**options_dict)
---
+++ style.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+def color_signs(s):
+    '''Color positive values green, negative values red, zero blue'''
+    zeros=np.where(s==0)
+    s=np.where(s>0, "color: green", "color: red")
+    s[zeros]="color: blue"
+    return s
+
+
+def style_df(a, metacols):
+    a_styled = a.style.set_properties(**{'text-align': 'center'})#.hide_index("ID") outdated pandas
+    a_styled = a_styled.set_table_styles([dict(selector='th', props=[('text-align', 'center')])]) # centering index name
+    a_styled = a_styled.format("{:.1f}",subset=['Score'])
+    #a_styled = a_styled.format("{:+.0f}",subset=['Diff.'])
+    a_styled = a_styled.format("{:.0f}",subset=['Rank','Votes'])
+    if "Runtime" in metacols: a_styled = a_styled.format("{:.0f}",subset=['Runtime'])
+    if "Episodes" in metacols: a_styled = a_styled.format("{:.0f}",subset=['Episodes'])
+    #a_styled.apply(color_signs, axis=0, subset=['Diff.'])
+    return a_styled
+
+
+def path_to_tmdb_image_html(path, width=120):
+    return f'<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2{path}" style="width:{width}px">'
